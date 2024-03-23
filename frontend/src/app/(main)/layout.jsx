fb8085b3a19eeb432a7615acbf1f242a54739599
m-triple-m <triplem656@gmail.com>
2024-03-23 10:20:54 +0530

    ..

--- frontend/src/app/(main)/layout.jsx
+++ frontend/src/app/(main)/layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     return (
         <CartProvider>
             <BrowseProvider>
-                {/* <Navbar /> */}
+                <Navbar />
                 {children}
             </BrowseProvider>
         </CartProvider>
