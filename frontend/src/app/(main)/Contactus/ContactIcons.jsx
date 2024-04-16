a831ecc2d76b69d6deda4e1d2dea98021b6d6aca
rakchitseth <rakchitseth@gmail.com>
2024-04-16 11:28:04 +0530

    contact

--- frontend/src/app/(main)/Contactus/ContactIcons.jsx
+++ frontend/src/app/(main)/Contactus/ContactIcons.jsx
@@ -20,10 +20,10 @@ function ContactIcon({ icon: Icon, title, description, ...others }) {
 }
 
 const MOCKDATA = [
-    { title: 'Email', description: 'hello@mantine.dev', icon: IconAt },
-    { title: 'Phone', description: '+49 (800) 335 35 35', icon: IconPhone },
-    { title: 'Address', description: '844 Morris Park avenue', icon: IconMapPin },
-    { title: 'Working hours', description: '8 a.m. – 11 p.m.', icon: IconSun },
+    { title: 'Email', description: 'cksewachikanindustries.verify@gmail.com', icon: IconAt },
+    { title: 'Phone', description: '+91 9696673056', icon: IconPhone },
+    { title: 'Address', description: '314/67 Mirza Mandi Chowk,Lucknow', icon: IconMapPin },
+    { title: 'Working hours', description: '8 a.m. – 8 p.m.', icon: IconSun },
 ];
 
 export function ContactIconsList() {
