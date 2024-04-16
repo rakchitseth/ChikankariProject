48548d685cd565d2592e153cc1b6397a46f90a76
rakchitseth <rakchitseth@gmail.com>
2024-04-16 09:32:19 +0530

    footer

---
+++ frontend/src/app/footer/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './about.css'; // Import the CSS file
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1 className="about-title">About Us</h1>
+      <p className="about-text">We are a company dedicated to providing the best service to our customers.</p>
+    </div>
+  );
+};
+
+export default About;
